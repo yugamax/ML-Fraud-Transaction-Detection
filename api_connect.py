1c39291a04e20ba8ad6b7f48bf2b9097b73aa1cf
yugamax <ayp14111@gmail.com>
2025-06-03 02:51:33 +0530

    Change uvicorn host to allow external access

--- api_connect.py
+++ api_connect.py
@@ -164,4 +164,4 @@ def predict(data: Transaction_data, db: Session = Depends(get_db)):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
